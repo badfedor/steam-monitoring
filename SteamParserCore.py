8dc89de0ebddace454e97fd39a1459725259b2cb
Fedor <apache32@yandex.ru>
2019-05-30 19:00:11 +0300

    Update SteamParserCore.py

--- SteamParserCore.py
+++ SteamParserCore.py
@@ -1,4 +1,4 @@
-import json, urllib3, openpyxl
+import json, urllib3, openpyxl, threading
 from openpyxl import Workbook
 from datetime import datetime
 
@@ -6,85 +6,80 @@ STEAM_API_KEY = 'YOUR_STEAM_API_KEY'
 
 SteamApi = urllib3.PoolManager()
 
-SteamApiUserId = STEAM_USER_ID
+SteamPlayersToMonitor = [STEAM_USER_IDS]
 
-SteamApiUrl = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + STEAM_API_KEY + '&steamids=' + str(SteamApiUserId)
+def GetPlayersInfo():
 
-SteamApiData = SteamApi.request('GET', SteamApiUrl)
-SteamApiData = SteamApiData.data.decode('utf-8')
+    threading.Timer(60.0, GetPlayersInfo).start()
 
-#SteamApiData = json.dumps(SteamApiData)
-SteamApiData = json.loads(SteamApiData)
+    for SteamApiUserId in SteamPlayersToMonitor:
 
-for Player in SteamApiData["response"]["players"]:
+        SteamApiUrl = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + STEAM_API_KEY + '&steamids=' + str(SteamApiUserId)
 
-    print(Player["steamid"], Player["personaname"])
-    
-    PlayerStatus = int(Player["personastate"])
-    if PlayerStatus == 0:
-        print('Offline')
-        PlayserStatusText = 'Offline'
-    elif PlayerStatus == 1:
-        print('Online')
-        PlayserStatusText = 'Online'
-    elif PlayerStatus == 2:
-        print('Busy')
-        PlayserStatusText = 'Busy'
-    elif PlayerStatus == 3:
-        print('Away')
-        PlayserStatusText = 'Away'
-    elif PlayerStatus == 4:
-        print('Snooze')
-        PlayserStatusText = 'Snooze'
-    elif PlayerStatus == 5:
-        print('looking to trade')
-        PlayserStatusText = 'looking to trade'
-    elif PlayerStatus == 6:
-        print('looking to play')
-        PlayserStatusText = 'looking to play'
-    else:
-        print('Error')
-        PlayserStatusText = 'Error'
+        SteamApiData = SteamApi.request('GET', SteamApiUrl)
+        SteamApiData = SteamApiData.data.decode('utf-8')
 
-    try:
-        print(Player["gameid"], Player["gameextrainfo"])
-        PlayerGameId = Player["gameid"]
-        PlayerGameExtraInfo = Player["gameextrainfo"]
-    except:
-        print("No data about game")
-        PlayerGameId = ' '
-        PlayerGameExtraInfo = ' '
+        #SteamApiData = json.dumps(SteamApiData)
+        SteamApiData = json.loads(SteamApiData)
 
-    try:
-        print(Player["gameserverip"])
-        PlayerGameServerIp = Player["gameserverip"]
-    except:
-        print("No data about server user currently playing")
-        PlayerGameServerIp = ' '
+        for Player in SteamApiData["response"]["players"]:
+            
+            PlayerStatus = int(Player["personastate"])
+            if PlayerStatus == 0:
+                PlayserStatusText = 'Offline'
+            elif PlayerStatus == 1:
+                PlayserStatusText = 'Online'
+            elif PlayerStatus == 2:
+                PlayserStatusText = 'Busy'
+            elif PlayerStatus == 3:
+                PlayserStatusText = 'Away'
+            elif PlayerStatus == 4:
+                PlayserStatusText = 'Snooze'
+            elif PlayerStatus == 5:
+                PlayserStatusText = 'looking to trade'
+            elif PlayerStatus == 6:
+                PlayserStatusText = 'looking to play'
+            else:
+                print('Error getting Status, Id: ' + str(SteamApiUserId))
+                PlayserStatusText = 'Error'
 
-print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            try:
+                PlayerGameId = Player["gameid"]
+                PlayerGameExtraInfo = Player["gameextrainfo"]
+            except:
+                PlayerGameId = ' '
+                PlayerGameExtraInfo = ' '
 
-ExcelTitle = ['year', 'month', 'day', 'hour', 'min', 'steamid', 'name', 'statuscode','status', 'gameid', 'gameinfo', 'gameserverip']
-ExcelStatsRow = [datetime.now().strftime('%Y'), datetime.now().strftime('%m'), datetime.now().strftime('%d'), 
-                datetime.now().strftime('%H'), datetime.now().strftime('%M'),
-                Player["steamid"], Player["personaname"], Player["personastate"], PlayserStatusText, PlayerGameId, PlayerGameExtraInfo, PlayerGameServerIp]
+            try:
+                print(Player["gameserverip"])
+                PlayerGameServerIp = Player["gameserverip"]
+            except:
+                PlayerGameServerIp = ' '
 
-def createExcelFile():
-    cr_Wb = Workbook()
-    cr_Ws = cr_Wb.active
-    cr_Ws.append(ExcelTitle)
-    cr_Wb.save(str(SteamApiUserId)+'.xlsx')
+        ExcelTitle = ['year', 'month', 'day', 'hour', 'min', 'steamid', 'name', 'statuscode','status', 'gameid', 'gameinfo', 'gameserverip']
+        ExcelStatsRow = [datetime.now().strftime('%Y'), datetime.now().strftime('%m'), datetime.now().strftime('%d'), 
+                        datetime.now().strftime('%H'), datetime.now().strftime('%M'),
+                        Player["steamid"], Player["personaname"], Player["personastate"], PlayserStatusText, PlayerGameId, PlayerGameExtraInfo, PlayerGameServerIp]
 
-def appendExcelStatsRow():
-    curExcel = openpyxl.load_workbook(str(SteamApiUserId)+'.xlsx')
-    curExcelWs = curExcel.active
-    curExcelWs.append(ExcelStatsRow)
-    curExcel.save(str(SteamApiUserId)+'.xlsx')
+        def createExcelFile():
+            cr_Wb = Workbook()
+            cr_Ws = cr_Wb.active
+            cr_Ws.append(ExcelTitle)
+            cr_Wb.save(str(SteamApiUserId)+'.xlsx')
 
-try:
-    appendExcelStatsRow()
-except:
-    createExcelFile()
-    print('New user, created a new excel file')
-    appendExcelStatsRow()
+        def appendExcelStatsRow():
+            curExcel = openpyxl.load_workbook(str(SteamApiUserId)+'.xlsx')
+            curExcelWs = curExcel.active
+            curExcelWs.append(ExcelStatsRow)
+            curExcel.save(str(SteamApiUserId)+'.xlsx')
 
+        try:
+            appendExcelStatsRow()
+        except:
+            createExcelFile()
+            print('New user, Id: ' + str(SteamApiUserId) + ', created a new excel file')
+            appendExcelStatsRow()
+
+    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + 'Info get SUCCESS, users total: ' + str(len(SteamPlayersToMonitor)))
+
+GetPlayersInfo()
